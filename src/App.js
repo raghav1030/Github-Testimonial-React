dd6f926746c0c003eca10cca5d4de34ec3a2a3e5
raghav1030 <raghav123ggandhi@gmail.com>
2023-04-03 19:19:21 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import reviews from "./data";
+import Testimonials from "./Components/Testimonials";
+
+const App = () => {
+
+
+  return(
+  <div className="flex flex-col h-screen w-screen justify-center items-center bg-gray-200  ">
+      <div className="text-center  " >
+          <h1 className="text-4xl font-bold  ">Our Testimonials</h1>
+          <div className="bg-violet-400 h-[4px] w-1/5 mx-auto mt-1  "></div>
+          <Testimonials reviews = {reviews}></Testimonials>
+      </div>
+  </div>
+
+  ) 
+};
+
+export default App;
